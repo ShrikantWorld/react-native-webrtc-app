ddd2bcff203842a6b7337cef09e1aa8849fe13c4
ShrikantWorld <shrikantkgacche@gmail.com>
2024-09-09 21:24:17 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,25 +1,20 @@
 const path = require('path');
 const { createServer } = require('http');
+
 const express = require('express');
-const { initIO } = require('./socket');
+const { getIO, initIO } = require('./socket');
 
 const app = express();
-const server = createServer(app);
 
-// Serve static files from 'public' directory
-//app.use('/', express.static(path.join(__dirname, '../public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
-// Initialize WebSocket
-initIO(server);
+const httpServer = createServer(app);
 
-app.get('/', (req, res) => {
-  res.send('Signaling server is running.');
-});
+let port = process.env.PORT || 3500;
 
-module.exports = (req, res) => {
-  // Vercel serverless function handler
-  server.listen(process.env.PORT || 3500, () => {
-    console.log('Server started');
-  });
-  app(req, res);
-};
+initIO(httpServer);
+
+httpServer.listen(port)
+console.log("Server started on ", port);
+
+getIO();
